59dee9ef275ba22ca4eac61f71375e37533bc52f
matteopolak <matteopolak@hotmail.com>
2024-04-02 17:16:56 -0400

    chore: reorder projects

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -10,16 +10,6 @@ export type Project = {
 
 export const projects = [
 	{
-		title: 'Crave',
-		thumbnail: '/images/projects/crave.webp',
-		date: 'December 2023',
-		description:
-			'Crave is a YouTube-like recipe explorer with semantic recommendations and powerful search. It allows users to find recipes based on their ingredients, and it also recommends recipes based on their previous searches. It also includes a powerful search engine that allows users to search for recipes based on their ingredients, their preparation time, and even their nutritional value.',
-		repository: 'https://github.com/matteopolak/crave',
-		website: 'https://crave.matteopolak.com',
-		tags: ['Svelte', 'TypeScript', 'Drizzle', 'PostgreSQL', 'PyTorch', 'tRPC'],
-	},
-	{
 		title: 'Quasi',
 		thumbnail: '/images/projects/quasi.webp',
 		date: 'November 2023',
@@ -36,6 +26,16 @@ export const projects = [
 			'A solo GPU- and CPU-capable Bitcoin miner with block-aware mining and a SHA256 compute shader. It is designed to be efficient and fast, with a focus on performance and ease of use.',
 		repository: 'https://github.com/matteopolak/miner',
 		tags: ['Rust', 'Blockchain', 'Hashing', 'WGSL', 'wgpu'],
+	},
+	{
+		title: 'Crave',
+		thumbnail: '/images/projects/crave.webp',
+		date: 'December 2023',
+		description:
+			'Crave is a YouTube-like recipe explorer with semantic recommendations and powerful search. It allows users to find recipes based on their ingredients, and it also recommends recipes based on their previous searches. It also includes a powerful search engine that allows users to search for recipes based on their ingredients, their preparation time, and even their nutritional value.',
+		repository: 'https://github.com/matteopolak/crave',
+		website: 'https://crave.matteopolak.com',
+		tags: ['Svelte', 'TypeScript', 'Drizzle', 'PostgreSQL', 'PyTorch', 'tRPC'],
 	},
 	{
 		title: 'StatPixel',
